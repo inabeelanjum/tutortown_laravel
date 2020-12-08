762fcaecb902b55f9e8863904be3becc34e4525a
Nadeem.I <c2nadeem@gmail.com>
2020-12-08 18:26:44 +0500

    modified authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Auth;
 class AuthController extends Controller
 {
+    
     public function login(Request $request) {
         $client = new \GuzzleHttp\Client();
         try {
