c92cf15fefb97cfd07e0339e13d9acc59f35d347
Nadeem.I <c2nadeem@gmail.com>
2020-12-12 20:46:42 +0500

    added distance calculation method

--- app/Http/Controllers/search.php
+++ app/Http/Controllers/search.php
@@ -11,6 +11,12 @@ class search extends Controller
 {
   public function index()
   {
+	$lat1 = '33.627603325927424';    
+	$lang1 = '73.03328330523966' ;   
+	$lat2 = '34.00443441471939' ;   
+	$lang2 = '73.76972818372637' ;   
+	echo $this->distance($lat1, $lang1, $lat2, $lang2, 'K');
+	exit;
     if(Auth::check())
     {
       return view('layout/home');
@@ -21,22 +27,39 @@ class search extends Controller
   }
   public function searchtutor(Request $req)
   {
-    $s=$req->search_item;
-   $tutors=profile::query()
-    ->where('subj1', 'LIKE', "%{$s}%") 
-    ->orWhere('subj2', 'LIKE', "%{$s}%") 
-    ->orWhere('subj3', 'LIKE', "%{$s}%")
-    ->orWhere('subj4', 'LIKE', "%{$s}%")
-    ->orWhere('subj5', 'LIKE', "%{$s}%")
-    ->orWhere('subj6', 'LIKE', "%{$s}%")    
-    ->get();
+	
+		$s=$req->search_item;
+		$tutors=profile::query()
+			->where('subj1', 'LIKE', "%{$s}%") 
+			->orWhere('subj2', 'LIKE', "%{$s}%") 
+			->orWhere('subj3', 'LIKE', "%{$s}%")
+			->orWhere('subj4', 'LIKE', "%{$s}%")
+			->orWhere('subj5', 'LIKE', "%{$s}%")
+			->orWhere('subj6', 'LIKE', "%{$s}%")    
+			->get();
 
-    if( $req->is('api/*')){
-      return ['status' => true, 'data' => $tutors];
-  } else {
-    return view('layout.result', ['tutors' => $tutors]);
-  }
+		if( $req->is('api/*')){
+			return ['status' => true, 'data' => $tutors];
+		} else {
+			return view('layout.result', ['tutors' => $tutors]);
+		}
     
     
   }
+  	function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+		$theta = $lon1 - $lon2;
+		$dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+		$dist = acos($dist);
+		$dist = rad2deg($dist);
+		$miles = $dist * 60 * 1.1515;
+		$unit = strtoupper($unit);
+	
+		if ($unit == "K") {
+			return ($miles * 1.609344);
+		} else if ($unit == "N") {
+			return ($miles * 0.8684);
+		} else {
+			return $miles;
+		}
+  	}
 }
